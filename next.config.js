51e144a89eb22142a85f54f79485edc3b89dcead
Gautam Jain <astrogeek77@gmail.com>
2023-09-05 23:35:46 +0530

    finished app with minor code and style bugs

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.dummyjson.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    domains: ['i.dummyjson.com'],
+  },
+}
